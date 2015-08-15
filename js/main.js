7bef70910cca03034b4ffbf0fb74102b12b40731
Zikani Nyirenda Mwase <zikani@creditdatamw.com>
2015-08-16 00:22:13 +0200

    Add initial scaffolding for artists page

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(function() {
+    'use strict';
+    var $artistSlider = $('#artistSlider');
+    var $artistContent = $('#content');
+    var $artistSliderItem = $('.artist-slider-item');
+        
+    function loadArtistDetails(event) {
+        // We will be in the context of the currently click item so we can
+        // extract the data-url
+        var dataUrl = $(this).attr('data-url');
+        var url = ["artists", dataUrl + ".html"].join("/");
+        return  $.get(url)
+         .success(function(data) {
+            $artistContent.html(data);
+         })
+         .fail(function() {
+            $artistContent.html('Failed to load artist page');
+         });
+    }
+    
+    $artistSliderItem.on('click', loadArtistDetails);
+});
